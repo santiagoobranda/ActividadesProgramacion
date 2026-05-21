e2c6849227b58dd5def274dc89394efe8084c364
santiagoobranda <santiago.branda6@gmail.com>
2026-05-21 15:51:54 -0300

    Update divisores.py

--- divisores.py
+++ divisores.py
@@ -1,10 +1,8 @@
 numero = int(input("Ingrese un número: "))
-
 contador = 0
-
 for i in range(1, numero + 1):
-    if numero % i == 0:
+    if numero / i == numero // i:
         print(i)
         contador += 1
-
+        
 print("Cantidad de divisores:", contador)
