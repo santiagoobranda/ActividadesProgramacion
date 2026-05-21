421f76d87cbf31649e8333f0df6bfc2372a15e49
santiagoobranda <santiago.branda6@gmail.com>
2026-05-21 15:54:29 -0300

    Update cantidad_primos.py

--- cantidad_primos.py
+++ cantidad_primos.py
@@ -1,11 +1,9 @@
 numero = int(input("Ingrese un número: "))
-
 cantidad_primos = 0
-
 for i in range(1, numero + 1):
     divisores = 0
     for j in range(1, i + 1):
-        if i % j == 0:
+        if i / j == i // j:
             divisores += 1
     if divisores == 2:
         print(i)
