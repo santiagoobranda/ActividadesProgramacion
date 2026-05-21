36e4556c808e5119c16cbbd47bfc4ee2fc558897
santiagoobranda <santiago.branda6@gmail.com>
2026-05-21 17:50:58 -0300

    Update and rename primo.py to Integradores/primo.py

--- Integradores/primo.py
+++ Integradores/primo.py
@@ -1,9 +1,7 @@
 numero = int(input("Ingrese un número: "))
-
 contador = 0
-
 for i in range(1, numero + 1):
-    if numero % i == 0:
+    if numero / i == numero // i:
         contador += 1
 if contador == 2:
     print("Es primo")
